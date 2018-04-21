547b8442f338bae2fa03197e3ebae0d5f9a397d5
joao.arvins <joao.arvins@gmail.com>
2018-04-21 16:11:50 +0100

    Identation fix for Players page

--- client/src/pages/Players.jsx
+++ client/src/pages/Players.jsx
@@ -6,49 +6,50 @@ import logo from '../logo.svg';
 import '../App.css';
 
 class Players extends Component {
-  componentDidMount() {
-    this.props.getPlayers();
-  }
+    componentDidMount() {
+        this.props.getPlayers();
+    }
 
-  render() {
-    const { isLoading, players } = this.props;
+    render() {
+        const { isLoading, players } = this.props;
 
-    const playersOutput = players.map(player => (
-      <li key={player.shortname}>{player.fullname}</li>
-    ));
-    const output = isLoading ? (
-      <img src={logo} className="App-logo" alt="logo" />
-    ) : (
-      playersOutput
-    );
+        const playersOutput = players.map(player => (
+            <li key={player.shortname}>{player.fullname}</li>
+        ));
 
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Players</h2>
-        </div>
-        {output}
-      </div>
-    );
-  }
+        const output = isLoading ? (
+            <img src={logo} className="App-logo" alt="logo" />
+        ) : (
+            playersOutput
+        );
+
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h2>Players</h2>
+                </div>
+                {output}
+            </div>
+        );
+    }
 }
 
 Players.propTypes = {
-  getPlayers: PropTypes.func.isRequired,
-  isLoading: PropTypes.bool,
-  players: PropTypes.arrayOf(PropTypes.object),
+    getPlayers: PropTypes.func.isRequired,
+    isLoading: PropTypes.bool,
+    players: PropTypes.arrayOf(PropTypes.object),
 };
 
 Players.defaultProps = {
-  isLoading: false,
-  players: [],
+    isLoading: false,
+    players: [],
 };
 
 const mapStateToProps = state => ({
-  players: state.resource.players,
+    players: state.resource.players,
 });
 
 export default connect(mapStateToProps, {
-  getPlayers,
+    getPlayers,
 })(Players);
