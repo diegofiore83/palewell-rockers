4c39c424683c809b7c1ab0b7825bd2e28cc077be
Diego Fiore <diegofiore83@hotmail.com>
2018-04-22 09:10:31 +0100

    adding header and footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
 import Home from './pages/Home';
 import Players from './pages/Players';
 import News from './pages/News';
 
 const App = () => (
-    <Switch>
-        <Route component={Home} exact path="/" />
-        <Route component={Players} path="/players" />
-        <Route component={News} path="/news" />
-    </Switch>
+    <Fragment>
+        <Header />
+        <Switch>
+            <Route component={Home} exact path="/" />
+            <Route component={Players} path="/players" />
+            <Route component={News} path="/news" />
+        </Switch>
+        <Footer />
+    </Fragment>
 );
 
 export default App;
