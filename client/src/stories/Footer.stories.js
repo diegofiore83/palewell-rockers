570800d0c9548a17e744bb369727e2c7246d346c
Diego Fiore <diegofiore83@hotmail.com>
2018-04-22 18:40:19 +0100

    remove storybook

--- client/src/stories/Footer.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-
-import Footer from '../components/layout/Footer';
-import { footerNavigation } from '../config/layout';
-
-storiesOf('Footer', module).addWithJSX('basic', () => (
-    <Footer links={footerNavigation} />
-));
--- client/src/stories/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import { storiesOf } from '@storybook/react';
-
-import { Welcome } from '@storybook/react/demo';
-
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome />);
