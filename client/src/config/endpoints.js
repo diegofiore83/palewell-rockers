7ba931a0df743d805dffa7e45c0db6aefbf76172
Diego Fiore <diegofiore83@hotmail.com>
2019-11-13 14:29:34 +0000

    Update endpoints

--- client/src/config/endpoints.js
+++ client/src/config/endpoints.js
@@ -1,9 +1,9 @@
-export const endpoints = {
-    api: 'http://localhost:5000/api',
-    assets: 'http://palewell-rockers.co.uk/assets',
-};
-
 // export const endpoints = {
-//     api: '/data/api',
-//     assets: '/assets',
+//     api: 'http://localhost:5000/api',
+//     assets: 'http://palewell-rockers.co.uk/assets',
 // };
+
+export const endpoints = {
+    api: '/data/api',
+    assets: '/assets',
+};
